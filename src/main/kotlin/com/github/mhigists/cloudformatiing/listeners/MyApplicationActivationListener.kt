f294e9712ff15d53c89c6436ec8d3243fa6e038d
GitHub Action <action@github.com>
2025-03-15 18:38:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/mhigists/cloudformatiing/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/mhigists/cloudformatiing/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.mhigists.cloudformatiing.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
